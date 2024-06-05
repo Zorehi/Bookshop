b699853478e19fd92048adcef09425de1acdbc13
Legrix Jeremy <jeremy.legrix@hotmail.com>
2024-06-05 15:49:20 +0200

    API KEY dans le ficher config.properties

--- src/main/java/fr/univtours/polytech/bookshop/dao/ExchangeRateDAOImpl.java
+++ src/main/java/fr/univtours/polytech/bookshop/dao/ExchangeRateDAOImpl.java
@@ -7,24 +7,40 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.MediaType;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Map;
+import java.util.Properties;
 
 @Stateless
 public class ExchangeRateDAOImpl implements ExchangeRateDAO {
-    private static String URL ="https://v6.exchangerate-api.com";
+    private static String url ="https://v6.exchangerate-api.com";
 
     public Map<String, Double> getConversion(String currency) {
         // Instanciation du client.
         Client client = ClientBuilder.newClient();
 
         // On indique l'URL du Web Service.
-        WebTarget target = client.target(URL);
+        WebTarget target = client.target(url);
+
+        Properties properties = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Le fichier config.properties n'a pas été trouvé.");
+            }
+            // Charger les propriétés depuis le fichier
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+
 
         // On indique le "end point" (on aurait aussi pu directement le mettre dans
         // l'URL).
         // C'est également avec cette méthode qu'on pourrait ajouter des "path
         // parameters" si besoin.
-        target = target.path("v6/4ac297b860f208ebd39c01d5/latest/" + currency);
+        target = target.path("v6/" + properties.get("apikey_exchangerate") + "/latest/" + currency);
 
         // On appelle le WS en précisant le type de l'objet renvoyé, ici un
         // WsOpenLibrary.
